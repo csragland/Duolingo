d5adbaa0ad2246a81761548686596330ff4cab8e
Hailey Hultquist <hailey@Haileys-MacBook-Pro.local>
2022-09-05 14:21:02 -0700

    template

--- process.py
+++ process.py
@@ -1,3 +1,19 @@
-# is_known_language == True -> answer in english, is_known_language == False -> response in high_valyrian
-def humanizer(answer, is_known_language, challenge_type, skill_level, course_percentage):
-    return answer, 0
+# sentence translation
+# Inputs: (string) answer
+#       (boolean) is answer in known language,
+#       (int 0-5) skill_level
+#       (float) course_percentage
+# Outputs: (string) modified answer
+#       (float) how many seconds to wait
+
+def human_sentence_translation(answer, known_language, skill_level, course_percentage):
+    return (answer, 0)
+
+# multiple choice
+# Inputs: (int 0-5 skill_level)
+#       (float) course_percentage
+# Outputs: (boolean) whether you will get it right
+#       (float) how many seconds to wait
+
+def human_multiple_choice(skill_level, course_percentage):
+    return (true, 0)
